1ac4de4d27adfbd886f195280a3971d97387ea09
mrquanfpt <37369536+mrquanfpt@users.noreply.github.com>
2018-11-02 09:05:48 +0700

    Finish paging

--- Dido_Store_2/Controllers/ProductController.cs
+++ Dido_Store_2/Controllers/ProductController.cs
@@ -22,15 +22,35 @@ namespace Dido_Store_2.Controllers
             return PartialView(model);
         }
 
-        public ActionResult ProductBranch(long branchID)
+        public ActionResult ProductBranch(long branchID, int page = 1, int pageSize = 1)
         {
             var branch = new BranchDao().ViewDetail(branchID);
-            return View(branch);
+            ViewBag.Branch = branch;
+            int totalRecord = 0;
+            var model = new ProductDao().ListByBranch(branchID, ref totalRecord, page, pageSize);
+
+            ViewBag.Total = totalRecord;
+            ViewBag.Page = page;
+
+            int maxPage = 5;
+            int totalPage = 0;
+
+            totalPage = (int)Math.Ceiling((double)(totalRecord / pageSize));
+            ViewBag.TotalPage = totalPage;
+            ViewBag.MaxPage = maxPage;
+            ViewBag.First = 1;
+            ViewBag.Last = maxPage;
+            ViewBag.Next = page + 1;
+            ViewBag.Prev = page - 1;
+
+            return View(model);
         }
 
         public ActionResult Detail(long productID)
         {
             var product = new ProductDao().GetProduct(productID);
+            ViewBag.Branch = new BranchDao().ViewDetail(product.BranchID.Value);
+            ViewBag.RelatedProduct = new ProductDao().ListRelatedProducts(productID);
             return View(product);
         }
     }
--- Model/Dao/ProductDao.cs
+++ Model/Dao/ProductDao.cs
@@ -16,25 +16,63 @@ namespace Model.Dao
             dbContext = new DidoStoreDbContext();
         }
 
-        //List all product
+        /// <summary>
+        /// List all product
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Product> ListAll()
         {
             return dbContext.Products.Where(x => x.Status == true).ToList();
         }
 
-        //List product by date
+        /// <summary>
+        /// List product by date
+        /// </summary>
+        /// <param name="top"></param>
+        /// <returns></returns>
         public List<Product> ListNewProducts(int top)
         {
             return dbContext.Products.OrderByDescending(x => x.CreatedDate).Take(top).ToList();
         }
 
-        //List discount product
+        /// <summary>
+        /// List product by branch
+        /// </summary>
+        /// <param name="branchID"></param>
+        /// <returns></returns>
+        public List<Product> ListByBranch(long branchID, ref int totalRecord, int pageIndex = 1, int pageSize = 2)
+        {
+            totalRecord = dbContext.Products.Where(x => x.BranchID == branchID).Count();
+            var model = dbContext.Products.Where(x => x.BranchID == branchID).OrderByDescending(x => x.CreatedDate).Skip((pageIndex -1) * pageSize).Take(pageSize).ToList();
+            return model;
+        }
+
+        /// <summary>
+        /// List discount product
+        /// </summary>
+        /// <param name="top"></param>
+        /// <returns></returns>
         public List<Product> ListPromotionProducts(int top)
         {
             return dbContext.Products.Where(x => x.PromotionPrice != null).OrderByDescending(x => x.CreatedDate).Take(top).ToList();
         }
 
-        //Get detail product
+        /// <summary>
+        /// List relate product
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        public List<Product> ListRelatedProducts(long productID)
+        {
+            var product = dbContext.Products.Find(productID);
+            return dbContext.Products.Where(x => x.ID != productID && x.BranchID == product.BranchID).ToList();
+        }
+
+        /// <summary>
+        /// Get detail product
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Product GetProduct(long id)
         {
             return dbContext.Products.Find(id);
