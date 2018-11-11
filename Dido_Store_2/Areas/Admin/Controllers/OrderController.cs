e5ae39ca5ae255a020f2dc65b537a2615f3da922
mrquanfpt <37369536+mrquanfpt@users.noreply.github.com>
2018-11-11 23:07:12 +0700

    order

---
+++ Dido_Store_2/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,59 @@
+﻿using Model.Dao;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dido_Store_2.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Admin/Order/Index
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 2)
+        {
+            var dao = new OrderDao();
+            IEnumerable<Order> model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.searchString = searchString;
+            return View(model);
+        }
+
+        [HttpPost]
+        public JsonResult ChangeStatus(int id)
+        {
+            var res = new ProductDao().ChangeStatus(id);
+            return Json(new
+            {
+                status = res
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Detail(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new OrderDao();
+                var res = dao.Update(product);
+                if (res)
+                {
+                    SetAlert("Cập nhật sản phẩm thành công.", "success");
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhận không thành công");
+                }
+            }
+            return View("Edit");
+        }
+
+        [NonAction]
+        public void SetViewBag(int? selectedId = null)
+        {
+            var dao = new BranchDao();
+            ViewBag.BranchID = new SelectList(dao.ListAll(), "ID", "BranchName", selectedId);
+        }
+    }
+}
--- Model/Dao/OrderDao.cs
+++ Model/Dao/OrderDao.cs
@@ -1,4 +1,6 @@
 ﻿using Model.EF;
+using Model.ViewModel;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,5 +24,50 @@ namespace Model.Dao
             return order.ID;
         }
 
+        public IEnumerable<Order> ListAll()
+        {
+            return dbContext.Orders.Where(x => x.Status == true).ToList();
+        }
+
+        public IEnumerable<Order> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<Order> model = dbContext.Orders;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.ShipName.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+
+        public bool? ChangeStatus(int id)
+        {
+            var order = dbContext.Orders.Find(id);
+            order.Status = !order.Status;
+            dbContext.SaveChanges();
+            return order.Status;
+        }
+
+        public OrderDetailViewModel ViewDetail(long id)
+        {
+            var model = (from a in dbContext.OrderDetails
+                         join b in dbContext.Products
+                         on a.ProductID equals b.ID
+                         where a.OrderID == id
+                         select new
+                         {
+                             id = b.ID,
+                             productName = b.Name,
+                             quantity = a.Quantity,
+                             price = b.Price,
+                         }).AsEnumerable().Select(x => new OrderDetailViewModel()
+                         {
+                             id = x.id,
+                             productName = x.productName,
+                             quantity = x.quantity,
+                             price = x.price,
+                             total = x.quantity * x.price
+                         });
+            return (OrderDetailViewModel)model;
+        }
     }
 }
